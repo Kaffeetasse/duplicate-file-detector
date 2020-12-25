e624c7f6c0f23013a5bc86e95c7104f7ba050942
Kaffeetasse <7276999+Kaffeetasse@users.noreply.github.com>
2020-12-25 14:16:38 +0100

    init

---
+++ src/find-duplicate-files/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace find_duplicate_files
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ExitPrompt();
+        }
+
+        private static void ExitPrompt()
+        {
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
