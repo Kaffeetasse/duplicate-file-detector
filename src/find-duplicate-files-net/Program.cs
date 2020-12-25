7544facbee232f37ebec67ce39de59e47206f9d0
Kaffeetasse <7276999+Kaffeetasse@users.noreply.github.com>
2020-12-25 15:44:15 +0100

    access mft

---
+++ src/find-duplicate-files-net/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Filesystem.Ntfs;
+
+namespace find_duplicate_files_net
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var drive = "e";
+            var listOfFiles = new List<string>();
+            var sw = new Stopwatch();
+
+            sw.Start();
+            var driveToAnalyze = new DriveInfo(drive);
+            using (var ntfsReader = new NtfsReader(driveToAnalyze, RetrieveMode.Minimal))
+            {
+                var nodes = ntfsReader.GetNodes(driveToAnalyze.Name);
+
+                foreach (var node in nodes)
+                    if ((node.Attributes & Attributes.System) == 0 &&
+                        (node.Attributes & Attributes.Archive) != 0 &&
+                        !node.FullName.Contains("$"))
+                    {
+                        // is file
+                        listOfFiles.Add(node.FullName);
+                        Console.WriteLine(node.FullName);
+                    }
+            }
+
+            sw.Stop();
+            var elapsedMs = sw.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine("Total files: {0}", listOfFiles.Count);
+            Console.WriteLine("Total ms: {0}", elapsedMs);
+            //ExitPrompt();
+        }
+
+        private static void ExitPrompt()
+        {
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
