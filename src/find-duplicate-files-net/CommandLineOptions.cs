cc882fe6ba100e82d489a1e7daab26ca30c6bf7a
Kaffeetasse <7276999+Kaffeetasse@users.noreply.github.com>
2020-12-28 14:38:25 +0100

    command line args + save result

---
+++ src/find-duplicate-files-net/CommandLineOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace find_duplicate_files_net
+{
+    public class CommandLineOptions
+    {
+        [Option('d', "drive", Required = true, HelpText = "Drive to analyze. eg: C, D, ...")]
+        public char DriveLetter { get; set; }
+
+        [Option('s', "save", Required = false, HelpText = "Save result to binary file.")]
+        public bool Save { get; set; }
+
+        [Option('o', "out", Required = false, HelpText = "Result in/out file name.", Default = "result.bin")]
+        public string SaveFile { get; set; }
+
+        [Option('r', "read", Required = false, HelpText = "Read from binary file.")]
+        public bool Read { get; set; }
+    }
+}
--- src/find-duplicate-files-net/Program.cs
+++ src/find-duplicate-files-net/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Filesystem.Ntfs;
+using System.Linq;
+using CommandLine;
+using GroBuf;
+using GroBuf.DataMembersExtracters;
 
 namespace find_duplicate_files_net
 {
@@ -9,26 +14,92 @@ namespace find_duplicate_files_net
     {
         internal static Stopwatch StopWatch = new Stopwatch();
 
+        internal static Serializer ResultSerializer =>
+            new Serializer(new PropertiesExtractor(), options: GroBufOptions.WriteEmptyObjects);
+
         private static void Main(string[] args)
         {
-            var drive = "h";
+            try
+            {
+                Parser.Default.ParseArguments<CommandLineOptions>(args)
+                    .WithParsed(RunOptions);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                ExitPrompt();
+            }
+        }
+
+        private static void RunOptions(CommandLineOptions opts)
+        {
+            var drive = opts.DriveLetter.ToString();
 
             StopWatch = new Stopwatch();
             StopWatch.Start();
 
-            var driveInfo = new DriveInfo(drive);
-            var ntfsReader = new NtfsReader(driveInfo, RetrieveMode.StandardInformations);
-            var fileHandler = new FileHandler(ntfsReader, driveInfo);
-
-            var duplicatesAfterChecksum = fileHandler.GetDuplicateFiles();
+            var duplicatesAfterChecksum = !opts.Read ? ScanDrive(drive) : ReadSaveFile(opts.SaveFile);
 
             StopWatch.Stop();
             var elapsedMs = StopWatch.ElapsedMilliseconds;
 
             Console.WriteLine("");
+            if (opts.Read) Console.WriteLine("Read from save file");
             Console.WriteLine("Total Time taken: {0} ms", elapsedMs);
             Console.WriteLine("Total duplicate files: {0}", duplicatesAfterChecksum.Count);
-            ExitPrompt();
+
+            if (!opts.Save) return;
+            CreateSaveFile(opts.SaveFile, duplicatesAfterChecksum);
+            Console.WriteLine("Created save file");
+        }
+
+        private static Dictionary<string, List<FoundFile>> ReadSaveFile(string saveFile)
+        {
+            if (string.IsNullOrWhiteSpace(saveFile))
+                throw new Exception("Save file was undefined");
+            if (!File.Exists(saveFile))
+                throw new Exception("Save file not found");
+            byte[] binaryData;
+            using (var reader = new BinaryReader(File.OpenRead(saveFile)))
+            {
+                const int bufferSize = 4096;
+                using (var ms = new MemoryStream())
+                {
+                    var buffer = new byte[bufferSize];
+                    int count;
+                    while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+                        ms.Write(buffer, 0, count);
+                    binaryData = ms.ToArray();
+                }
+            }
+
+            var serializer = ResultSerializer;
+            return serializer.Deserialize<Dictionary<string, List<FoundFile>>>(binaryData);
+        }
+
+        private static Dictionary<string, List<FoundFile>> ScanDrive(string drive)
+        {
+            var driveInfo = new DriveInfo(drive);
+            var ntfsReader = new NtfsReader(driveInfo, RetrieveMode.StandardInformations);
+            var fileHandler = new FileHandler(ntfsReader, driveInfo);
+
+            var duplicatesAfterChecksum = fileHandler.GetDuplicateFiles();
+            return duplicatesAfterChecksum;
+        }
+
+        private static void CreateSaveFile(string saveFile, Dictionary<string, List<FoundFile>> duplicates)
+        {
+            if (!duplicates.Any() || string.IsNullOrWhiteSpace(saveFile))
+                return;
+            var serializer = ResultSerializer;
+            var binaryData = serializer.Serialize(duplicates);
+            using (var writer = new BinaryWriter(File.Open(saveFile, FileMode.Create)))
+            {
+                writer.Write(binaryData);
+            }
         }
 
         private static void ExitPrompt()
